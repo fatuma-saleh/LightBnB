77365c4bd911075a17bf1aabbb64a803f2afc9fc
Fatuma <sfatex@gmail.com>
2021-10-14 00:56:02 -0400

    Add update and delete buttons for upcoming  reservations

--- LightBnB_WebApp/public/javascript/components/property_listings.js
+++ LightBnB_WebApp/public/javascript/components/property_listings.js
@@ -30,6 +30,18 @@ $(() => {
       const listing = propertyListing.createListing(property, isReservation);
       addListing(listing);
     }
+    
+    //checks to see if reservations have been added
+    if (isReservation) {
+      $('.update-button').on('click', function() {
+        const idData = $(this).attr('id').substring(16);
+        console.log(`update ${idData}`);          
+      })
+      $('.delete-button').on('click', function() {
+        const idData = $(this).attr('id').substring(16);
+        console.log(`delete ${idData}`);          
+      })
+    } 
   }
   window.propertyListings.addProperties = addProperties;
 
