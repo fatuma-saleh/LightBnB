ea4d73eb53594b9efb5474031a84450450e4e9cc
Fatuma <sfatex@gmail.com>
2021-10-16 20:24:30 -0400

    Add event handler for review details

--- LightBnB_WebApp/public/javascript/index.js
+++ LightBnB_WebApp/public/javascript/index.js
@@ -1,10 +1,34 @@
-$(() => {
+// $(() => {
+//   getAllListings().then(function( json ) {
+//     propertyListings.addProperties(json.properties);
+//     views_manager.show('listings');
+//      $('.reserve-button').on('click', function() {
+//       // (document).on('click','.reserve-button', function(){
+//       const idData = $(this).attr('id').substring(17);
+//     views_manager.show('newReservation', idData);
+//     })
+//   });
+// });
+
+$(() => { // jQuery shortcut for document.ready
   getAllListings().then(function( json ) {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
     $('.reserve-button').on('click', function() {
       const idData = $(this).attr('id').substring(17);
-    views_manager.show('newReservation', idData);
+      views_manager.show('newReservation', idData);
     })
+    
+    // $('.review_details').on('click', function() {
+    //   const idData = $(this).attr('id').substring(15);
+    //   console.log(idData);
+    // })
+    
+    
+    $('.review_details').on('click', function() {
+      const idData = $(this).attr('id').substring(15);
+      views_manager.show('showReviews', idData);
+    })
+
   });
 });
